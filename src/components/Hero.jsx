766d0418bdd3eeafe005d348211e5b4df49bdbda
JerryVincent <jerrynambalampara111@gmail.com>
2025-05-05 15:42:05 +0200

    updated cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ function Hero() {
                     <motion.h2 variants={childVariants} className='pb-2 text-4xl tracking-tighter lg:text-8xl'>Jerry Vincent</motion.h2>
                     <motion.span variants={childVariants} className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>Full Stack Developer</motion.span>
                     <motion.p variants={childVariants} className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>{HERO_CONTENT}</motion.p>
-                    <motion.a variants={childVariants} href="/resume.pdf" target='_blank' rel='noopener noreferrer' download={true} className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>
+                    <motion.a variants={childVariants} href="/JerryVincent_CV.pdf" target='_blank' rel='noopener noreferrer' download={true} className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'>
                     Download Resume</motion.a>
                 </motion.div>
             </div>
